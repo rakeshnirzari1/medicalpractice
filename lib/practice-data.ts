deb59e4e6373972b922997af78ecf422138a290d
rakeshnirzari1 <124740209+rakeshnirzari1@users.noreply.github.com>
2025-04-29 12:45:42 +1000

    Add files via upload

---
+++ lib/practice-data.ts
@@ -0,0 +1,28 @@
+// This file contains the practice data that you can customize for each client
+
+export const practiceData = {
+  practiceName: "undefined",
+  shortDescription: "undefined",
+  longDescription: `undefined`,
+  address: "undefined",
+  phone: "undefined",
+  email: "undefined",
+  openingHours: {
+    Monday: "8:00 - 18:00",
+    Tuesday: "8:00 - 18:00",
+    Wednesday: "8:00 - 18:00",
+    Thursday: "8:00 - 18:00",
+    Friday: "8:00 - 18:00",
+    Saturday: "9:00 - 14:00",
+    Sunday: "Closed",
+  },
+  socialLinks: {
+    facebook: "https://facebook.com",
+    instagram: "https://instagram.com",
+    twitter: "https://twitter.com",
+  },
+  services: undefined,
+  teamMembers: undefined,
+  heroContent: undefined,
+  aboutContent: undefined,
+}
