17cdfb460cbc79da5e6af651526522670dd398eb
rakeshnirzari1 <124740209+rakeshnirzari1@users.noreply.github.com>
2025-04-29 15:30:24 +1000

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -8,6 +8,7 @@ interface ThemeProviderProps {
   attribute?: string;
   defaultTheme?: string;
   enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
 }
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
